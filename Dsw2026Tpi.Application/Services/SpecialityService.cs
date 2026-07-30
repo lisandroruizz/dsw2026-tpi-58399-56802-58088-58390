c96bbebf995736f90aeecab9eca23c8b6fc1cfe7
Priscila Rocha <Priscila.Rocha@alu.frt.utn.edu.ar>
2026-07-30 02:28:58 -0300

    feat(application): implementar casos de uso de especialidades

---
+++ Dsw2026Tpi.Application/Services/SpecialityService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dsw2026Tpi.Application.Dtos;
+
+
+namespace Dsw2026Tpi.Application.Services
+{
+    internal class SpecialityService
+    {
+
+    }
+}
