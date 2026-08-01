0254c973e3e9a1c2b563ab0dfc2014828e25361c
Garcia Luca <Luca.Garcia@alu.frt.utn.edu.ar>
2026-08-01 17:01:47 -0300

    feat(application): definir DTO y contrato de citas

---
+++ Dsw2026Tpi.Application/Interfaces/IAppointmentService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dsw2026Tpi.Application.Dtos; 
+using Dsw2026Tpi.Domain.Entities; 
+
+namespace Dsw2026Tpi.Application.Interfaces;
+
+public interface IAppointmentService
+{ 
+    Task<AppointmentModel.Response> Create(AppointmentModel.Request request, long authenticatedDni);
+    Task<IReadOnlyCollection<AppointmentModel.Response>> GetPatientActive(long dni, long authenticatedDni);
+    Task Cancel(Guid id, long authenticatedDni);
+
+    Task<IReadOnlyCollection<AppointmentModel.Response>> GetByDate(DateOnly date);
+    Task<Pagination<AppointmentModel.Response>> Search(
+        int pageSize,
+        int pageIndex,
+        Guid? specialityId, 
+        Guid? doctorId, 
+        long? dni,
+        DateOnly? date);
+}
